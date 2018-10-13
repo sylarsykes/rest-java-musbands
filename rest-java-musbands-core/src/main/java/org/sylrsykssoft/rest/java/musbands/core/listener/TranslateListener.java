2eb3ab14f5f1667b08380fa3a066319dd865a81c
Juan <sylar.sykes@gmail.com>
2018-10-13 19:02:40 +0200

    Translatable module

--- rest-java-musbands-core/src/main/java/org/sylrsykssoft/rest/java/musbands/core/listener/TranslateListener.java
+++ rest-java-musbands-core/src/main/java/org/sylrsykssoft/rest/java/musbands/core/listener/TranslateListener.java
@@ -5,10 +5,11 @@ import java.util.Locale;
 import javax.persistence.PrePersist;
 
 import org.apache.commons.lang3.StringUtils;
+import org.sylrsykssoft.rest.java.musbands.core.domain.ITranslatable;
 import org.sylrsykssoft.rest.java.musbands.core.domain.Translate;
 
 /**
- * BaseAdminTranslatable entity entity listener.
+ * Translate listener.
  * 
  * @author juan.gonzalez.fernandez.jgf
  *
@@ -21,9 +22,28 @@ public class TranslateListener {
 	 * @param base
 	 */
 	@PrePersist
-	void onPrePersist(final Translate base) {
-		if (StringUtils.isEmpty(base.getLocale())) {
-			base.setLocale(Locale.UK.getLanguage());
+	void onPrePersist(final Object translatebleObject) {
+		if (translatebleObject instanceof ITranslatable) {
+			Translate translate = getTranslatable((ITranslatable) translatebleObject);
+
+			if (StringUtils.isEmpty(translate.getLocale())) {
+				translate.setLocale(Locale.UK.getLanguage());
+			}
 		}
+	}
+
+	/**
+	 * Getter audit
+	 * 
+	 * @param auditableObject
+	 * @return
+	 */
+	private Translate getTranslatable(final ITranslatable translatableObject) {
+		Translate translate = translatableObject.getTranslate();
+		if (translate == null) {
+			translate = new Translate();
+			translatableObject.setTranslate(translate);
+		}
+		return translate;
 	}
 }
