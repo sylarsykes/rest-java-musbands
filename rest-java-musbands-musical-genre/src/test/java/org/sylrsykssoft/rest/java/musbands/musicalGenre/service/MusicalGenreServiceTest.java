c28fcc00865a0fe0b53312b6dd2225aff7e107dc
Juan <sylar.sykes@gmail.com>
2018-10-28 13:16:53 +0100

    Defining Spring Security module

--- rest-java-musbands-musical-genre/src/test/java/org/sylrsykssoft/rest/java/musbands/musicalGenre/service/MusicalGenreServiceTest.java
+++ rest-java-musbands-musical-genre/src/test/java/org/sylrsykssoft/rest/java/musbands/musicalGenre/service/MusicalGenreServiceTest.java
@@ -8,8 +8,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
