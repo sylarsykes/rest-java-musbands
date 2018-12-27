2e626319152616095d04eff19aa08241f3c2098c
Juan <sylar.sykes@gmail.com>
2018-12-27 20:43:29 +0100

    Logger Aspect
    https://trello.com/c/JsxyQOIR

---
+++ common/aspects/src/main/java/org/sylrsykssoft/rest/java/musbands/aspects/logger/annotation/Logging.java
@@ -0,0 +1,53 @@
+package org.sylrsykssoft.rest.java.musbands.aspects.logger.annotation;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Class and method level annotation to turn on automatic logging. 
+ * Adding it to class or method enables logging for it.
+ * Annotation on method takes precedence over that on class.
+ * 
+ * @author juan.gonzalez.fernandez.jgf
+ *
+ */
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface Logging {
+	/**
+     * TRACE level of logging.
+     */
+    int TRACE = 0;
+
+    /**
+     * DEBUG level of logging.
+     */
+    int DEBUG = 1;
+
+    /**
+     * INFO level of logging.
+     */
+    int INFO = 2;
+
+    /**
+     * WARN level of logging.
+     */
+    int WARN = 3;
+
+    /**
+     * ERROR level of logging.
+     */
+    int ERROR = 4;
+
+    /**
+     * Level of logging.
+     */
+    int value() default Logging.INFO;
+
+}
